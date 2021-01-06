d15e3ba038e76fba2b2222476d838683a3f7d044
Márcio Habigzang Brufatto <mhbrufatto@gmail.com>
2021-01-06 16:50:41 -0300

    Initial Commit

---
+++ GoodList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodList
+//
+//  Created by Marcio Habigzang Brufatto on 06/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
